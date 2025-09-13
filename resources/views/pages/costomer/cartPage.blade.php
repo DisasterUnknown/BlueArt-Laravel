82b3defdbfb2b48267bffd0ea1379a944d4c9f19
DisasterUnknown <priyashanthafdo26@gmail.com>
2025-09-13 23:15:21 +0530

    added page titles for the pages

--- resources/views/pages/costomer/cartPage.blade.php
+++ resources/views/pages/costomer/cartPage.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-app-layout title="My Cart">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             My Cart
